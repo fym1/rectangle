a51e006a286bbf0bc915a84feb164532f4e4cae0
fym1 <1668576168@qq.com>
2020-02-28 13:59:58 +0800

    矩形计算器v0.3

--- rectangle.js
+++ rectangle.js
@@ -5,21 +5,15 @@ $(function(){
             $bthCal = $('#calculate'),
             $perimeter = $('#perimeter'),
             $area = $('#area');
-    //*calc button click event
-    function roundFractional(x, n) {
-        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-    }
+
     $bthCal.click(function(){
         //get value
-        var w = Number($width.val()),
-                h = Number($height.val());
+        var w = $width.val(),
+            h = $height.val();
         //calculate
-        var p = 2*(w+h),
-                a = w*h;
+        var r = new Rectangle(w,h);
         //output
-        $perimeter.val(p);
-        $area.val(a);
-        $perimeter.val(roundFractional(p, 1));
-        $area.val(roundFractional(a, 2));
+        $perimeter.val(r.perimeter());
+        $area.val(r.area());
     });
 });
